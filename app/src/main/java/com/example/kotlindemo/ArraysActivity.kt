e0a4e2dbdf7b70c6c0b2a757b9e6acc2204cb056
Kiran Prince IT <kiran.barvaliya@princeitsolution.com>
2024-09-16 17:46:42 +0530

    Kotlin Demo

--- app/src/main/java/com/example/kotlindemo/ArraysActivity.kt
+++ app/src/main/java/com/example/kotlindemo/ArraysActivity.kt
@@ -3,11 +3,16 @@ package com.example.kotlindemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class ArraysActivity : AppCompatActivity() {
+
+    lateinit var tv_array: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arrays)
+        tv_array=findViewById(R.id.tv_array)
+
 
         val fruits = arrayOf<String>("Apple", "Mango", "Banana", "Orange", "Apple")
 
@@ -15,7 +20,7 @@ class ArraysActivity : AppCompatActivity() {
         for( item in result ){
             println( item )
         }
-
+        tv_array.setText(result.toString())
         Log.d("TAG", "onCreate: "+result)
     }
 
